0088be7cddb3874b6b5b36aa793655397fc75d00
Mithra M S <36097660+mithrams07@users.noreply.github.com>
2022-05-08 21:03:19 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "access_key"{
+  default=  "<your-access-key>"
+}
+
+variable "secret_key"{
+  default= "<your-secret-key>"
+}
+
+variable "region"{
+  default= "ap-south-1"
+}
+
+variable "project" {
+  default = "honda"
+}
+
+variable "instance_ami" {
+  default = "ami-03b4429240c6dfff3"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
